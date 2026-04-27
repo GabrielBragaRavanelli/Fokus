0063b06dadf35adcaf3c4619ae65b0348152e30a
Gabriel Braga Ravanelli <gabrielravanelli1@gmail.com>
2026-04-27 20:30:50 -0300

    Aplicando o DOM:  Ao clicar nos botões, o fundo, o texto e a imagem mudam e, também, a musica começa a tocar.

--- script.js
+++ script.js
@@ -2,15 +2,57 @@ const html = document.querySelector("html")
 const focoBt = document.querySelector(".app__card-button--foco")
 const curtoBt = document.querySelector(".app__card-button--curto")
 const longoBt = document.querySelector(".app__card-button--longo")
+const banner = document.querySelector(".app__image")
+const titulo = document.querySelector(".app__title")
+const botoes = document.querySelectorAll(".app__card-button")
+const musicaFocoInput = document.querySelector(".alternar-musica")
+const musica = new Audio("/sons/luna-rise-part-one.mp3")
+musica.loop = true
 
-focoBt.addEventListener("click", () => {
-    html.setAttribute("data-contexto","foco")
+musicaFocoInput.addEventListener("change", () => {
+    if(musica.paused){
+        musica.play()
+    }else{
+        musica.pause()
+    }
 })
 
+focoBt.addEventListener("click", () => {
+    alterarContexto("foco")
+    focoBt.classList.add("active")
+})
+
+
 curtoBt.addEventListener("click",() => {
-    html.setAttribute("data-contexto","curto")
+    alterarContexto("curto")
+    curtoBt.classList.add("active")
 })
 
 longoBt.addEventListener("click",() => {
-    html.setAttribute("data-contexto" , "longo")
-})
+    alterarContexto("longo")
+    longoBt.classList.add("active")
+});
+
+function alterarContexto(contexto){
+    botoes.forEach(function(contexto){
+        contexto.classList.remove("active")
+    })
+    html.setAttribute("data-contexto",contexto)
+    const nomeImagem = contexto === "curto" ? "descanso-curto" : contexto === "longo" ? "descanso-longo" : contexto
+    banner.setAttribute("src",`./imagens/${nomeImagem}.png`)
+    switch(contexto){
+        case"foco":            
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case"curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br><strong class="app__title-strong">Faça uma pausa curta!`
+            break;
+        case"longo":
+            titulo.innerHTML = `Hora de voltar à superfície.<br><strong class="app__title-strong">
+Faça uma pausa longa.` 
+        default:
+            break;
+    }
+}
+
